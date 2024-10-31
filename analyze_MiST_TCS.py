7d0099e1786e6d431da34b7f6c7325765e9952d2
Vadim Gumerov <netuns@gmail.com>
2024-10-31 16:28:44 -0400

    Factor in MiST domain superfamily information

--- analyze_MiST_TCS.py
+++ analyze_MiST_TCS.py
@@ -11,24 +11,31 @@ USAGE = "\n\nThe script . \n" + \
 	". \n" + \
 	"python 	" + sys.argv[0] + '''
 	-h || --help               - help
-	-i || --ifile              - input file
+	-i || --ifile              - input file 1
+	-s || --sfile              - input file 2 (with MiST domain information)
 	-f || --ffile              - output file 1
 	-g || --gfile              - output file 2 
 '''
 
-#Variables controlled by the script parameters
-INPUT_FILE = None
+# Variables controlled by the script parameters
+INPUT_FILE1 = None
+INPUT_FILE2 = None
 OUTPUT_FILE1 = "genome_to_domain_data.tsv"
 OUTPUT_FILE2 = "genome_to_domain_comb_data.tsv"
 
-#Variables set within the script
+MIST_DOMAIN_TO_SUPERFAMILY = {}
+HIS_KINASE_DIM_DOMAINS = ["HisKA", "HisKA_2", "HisKA_3", "H-kinase_dim", "His_kinase"]
+HIS_KINASE_CATAL_DOMAINS = ["HATPase_c", "HATPase_c_2", "HATPase_c_5", "HWE_HK"]
+RESPONSE_REG_DOMAINS = ["Response_reg", "FleQ"]
+
+# Variables set within the script
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(filename=sys.argv[0].replace(".py", "") + "_log.txt", level=logging.INFO)
 
 def initialize(argv):
-	global INPUT_FILE, OUTPUT_FILE1, OUTPUT_FILE2
+	global INPUT_FILE1, INPUT_FILE2, OUTPUT_FILE1, OUTPUT_FILE2
 	try:
-		opts, args = getopt.getopt(argv[1:],"hi:f:g:",["help", "ifile=", "ffile=", "gfile="])
+		opts, args = getopt.getopt(argv[1:],"hi:s:f:g:",["help", "ifile=", "sfile=", "ffile=", "gfile="])
 		if len(opts) == 0:
 			raise getopt.GetoptError("Options are required\n")
 	except getopt.GetoptError as e:
@@ -40,7 +47,9 @@ def initialize(argv):
 				print(USAGE)
 				sys.exit()
 			elif opt in ("-i", "--ifile"):
-				INPUT_FILE = str(arg).strip()
+				INPUT_FILE1 = str(arg).strip()
+			elif opt in ("-s", "--sfile"):
+				INPUT_FILE1 = str(arg).strip()
 			elif opt in ("-f", "--ffile"):
 				OUTPUT_FILE1 = str(arg).strip()
 			elif opt in ("-g", "--gfile"):
@@ -50,44 +59,45 @@ def initialize(argv):
 		print("===========ERROR==========\n " + str(e) + USAGE)
 		sys.exit(2)
 
-HIS_KINASE_DIM_DOMAINS = ["HisKA", "HisKA_2", "HisKA_3", "H-kinase_dim", "His_kinase"]
-HIS_KINASE_CATAL_DOMAINS = ["HATPase_c", "HATPase_c_2", "HATPase_c_5", "HWE_HK"]
-RESPONSE_REG_DOMAINS = ["Response_reg", "FleQ"]
-
-
+def processInput():
+	global MIST_DOMAIN_TO_SUPERFAMILY
+	with open(INPUT_FILE2, "r") as iFile2:
+		for line in iFile2:
+			domain_info = line.split("\t")
+			MIST_DOMAIN_TO_SUPERFAMILY[domain_info[0]] = domain_info[3] 
 
 #Genome_id,NCBI_id,MiST_id,protein_length,domain_architecture,sensors_or_regulators,domain_counts,domain_combinations = [f for f in protein.strip().split("\t")]
-
-
-GENOME_TO_DOMAIN_DATA = {}
-GENOME_TO_DOMAIN_COMB_DATA = {}
-
 
 # domain_to_protein_count = {"dCache_1":5, "GAF": 2, ...}. This means that in a given genome dCache was found in 5 proteins and GAF in 2 of a considered system.
 # This does not count how many times a domain is present in a single protein.
 # domain_comb_to_protein_count = {"MEDS,PAS,PAS_3,PAS_4,PAS_9":2}. This means that a particualr domain combiation was found in 2 protins in the given genome
 def findDomainAndCombPrevalenceInProteins():
 	domain_to_protein_count = collections.defaultdict(int)
-	domain_comb_to_protein_count = collections.defaultdict(int) 
+	superfamily_to_protein_count = collections.defaultdict(int)
+
+	domain_comb_to_protein_count = collections.defaultdict(int)
+	superfamily_comb_to_protein_count = collections.defaultdict(int)
+
 	sensor_domain_comb = ""
+	sensor_superfamily_comb = {}
 	Genome_id_prev = None
-	with open(INPUT_FILE, "r") as iFile:
+	with open(INPUT_FILE1, "r") as iFile:
 		for protein in iFile:
-			#filed 6 has only uniqe domain names with indicated counts showing how many times a given domain is present in a given protein.
+			# filed 6 has only uniqe domain names with indicated counts showing how many times a given domain is present in a given protein.
+			# Domains are sorted alphabetically
 			# Ex., HATPase_c:1,HisKA_2:1,MEDS:1,PAS:1,PAS_3:2,PAS_4:1,PAS_9:1 
 			protein_record = protein.strip().split("\t")
 			Genome_id = protein_record[0]
 			domain_counts = protein_record[6].replace("<", "").replace(">", "")
-			#if records of a new genome began, save the previous genome data and update variables
+			sensor_domain_comb = ""
+			# if records of a new genome began, save the previous genome data and update variables
 			if Genome_id != Genome_id_prev:
 				if Genome_id_prev:
-					#GENOME_TO_DOMAIN_DATA[Genome_id_prev] = domain_to_protein_count
-					#GENOME_TO_DOMAIN_COMB_DATA[Genome_id_prev] = domain_comb_to_protein_count
-					with open (OUTPUT_FILE1, "a") as oFile1, open(OUTPUT_FILE2, "a"):
+					with open (OUTPUT_FILE1, "a") as oFile1, open(OUTPUT_FILE2, "a") as oFile2:
 						for domain,count in domain_to_protein_count.items():
-							oFile1.write("\t".join([Genome_id_prev, domain, count]) + "\n")
+							oFile1.write("\t".join([Genome_id_prev, domain, str(count)]) + "\n")
 						for domain_comb,count in domain_comb_to_protein_count.items():
-							oFile1.write("\t".join([Genome_id_prev, domain_comb, count]) + "\n")
+							oFile2.write("\t".join([Genome_id_prev, domain_comb, str(count)]) + "\n")
 
 				Genome_id_prev = Genome_id
 				domain_to_protein_count = collections.defaultdict(int)
@@ -96,12 +106,15 @@ def findDomainAndCombPrevalenceInProteins():
 			domains_and_counts = domain_counts.split(",")
 			for domain in domains_and_counts:
 				domain = domain.split(":")[0]
-				#if entry is a sensor domain
+				# if entry is a sensor domain
 				if domain not in HIS_KINASE_DIM_DOMAINS and domain not in HIS_KINASE_CATAL_DOMAINS:
 					domain_to_protein_count[domain]+=1
+					superfamily_to_protein_count[MIST_DOMAIN_TO_SUPERFAMILY[domain]]+=1
+
 					sensor_domain_comb = sensor_domain_comb + "," + domain
-			#count this domain combination
-			domain_comb_to_protein_count[sensor_domain_comb]+=1 
+					sensor_superfamily_comb[MIST_DOMAIN_TO_SUPERFAMILY[domain]] = None
+			# count this domain combination
+			domain_comb_to_protein_count[sensor_domain_comb.lstrip(",")]+=1 
 			
 
 def main(argv):

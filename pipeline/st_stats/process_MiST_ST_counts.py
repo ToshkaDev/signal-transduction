a86e316b37deac3abefdf2a084fcb345abee7702
Vadim Gumerov <netuns@gmail.com>
2025-12-18 17:16:57 -0500

    Add a script for ST counts

---
+++ pipeline/st_stats/process_MiST_ST_counts.py
@@ -0,0 +1,196 @@
+#!/usr/bin/python3
+
+import sys
+import getopt
+import http.client
+import json
+from collections import OrderedDict
+
+USAGE = "\n\nThe script queries MiST database via it's API for the counts of signal transduction proteins in genomes.\n\n" + \
+	"It produces ST info for each genome. \n\n" + \
+	"python 	" + sys.argv[0] + '''
+	-h || --help               - help
+	-i || --ifile              - input file
+	-o || --ofile              - output file
+	'''
+
+INPUT_FILE = None
+OUTPUT_FILE = None
+TIMEOUT_FILE = "timeout_genomeST_counts.txt"
+
+CHEA_FILE_OUTPUT = "CheA_across_genomes.txt"
+CHEB_FILE_OUTPUT = "CheB_across_genomes.txt"
+CHER_FILE_OUTPUT = "CheR_across_genomes.txt"
+CHEZ_FILE_OUTPUT = "CheZ_across_genomes.txt"
+CHED_FILE_OUTPUT = "CheD_across_genomes.txt"
+CHEV_FILE_OUTPUT = "CheV_across_genomes.txt"
+MCP_FILE_OUTPUT = "MCP_across_genomes.txt"
+
+def initialize(argv):
+	global INPUT_FILE, OUTPUT_FILE, OUTPUT_FILE2, ST_INPUT_FILE, TASK
+	try:
+		opts, args = getopt.getopt(argv[1:],"hi:o:",["help", "ifile=", "ofile="])
+		if len(opts) == 0:
+			raise getopt.GetoptError("Options are required\n")
+	except getopt.GetoptError as e:
+		print(("===========ERROR==========\n " + str(e) + USAGE))
+		sys.exit(2)
+	try:
+		for opt, arg in opts:
+			if opt in ("-h", "--help"):
+				print(USAGE)
+				sys.exit()
+			elif opt in ("-i", "--ifile"):
+				INPUT_FILE = str(arg).strip()
+			elif opt in ("-o", "--ofile"):
+				OUTPUT_FILE = str(arg).strip()
+				TASK = str(arg).strip()
+	except Exception as e:
+		print(("===========ERROR==========\n " + str(e) + USAGE))
+		sys.exit(2)
+
+
+TEMPLATE_TO_COUNTS = OrderedDict([("$total", 0), ("F1", 0), ("F2", 0), ("F3", 0), ("F4", 0), ("F5", 0), ("F6", 0), ("F7", 0), ("F8", 0), ("F9", 0), ("F10", 0), ("F11", 0), ("F12", 0), ("F13", 0), ("F14", 0), ("F15", 0), ("F16", 0), ("F17", 0), ("Acf", 0), ("Tfp", 0)])
+CHEA_TO_COUNTS = OrderedDict([("$total", 0), ("chew", 0), ("checx", 0), ("other", 0), ("F1", 0), ("F2", 0), ("F3", 0), ("F4", 0), ("F5", 0), ("F6", 0), ("F7", 0), ("F8", 0), ("F9", 0), ("F10", 0), ("F11", 0), ("F12", 0), ("F13", 0), ("F14", 0), ("F15", 0), ("F16", 0), ("F17", 0), ("Acf", 0), ("Tfp", 0)])
+CHED_TO_COUNTS = TEMPLATE_TO_COUNTS.copy()
+CHEZ_TO_COUNTS = TEMPLATE_TO_COUNTS.copy()
+CHEV_TO_COUNTS = TEMPLATE_TO_COUNTS.copy()
+CHEB_TO_COUNTS = OrderedDict([("$total", 0), ("MAC1", 0), ("MAC2", 0), ("F1", 0), ("F2", 0), ("F3", 0), ("F4", 0), ("F5", 0), ("F6", 0), ("F7", 0), ("F8", 0), ("F9", 0), ("F10", 0), ("F11", 0), ("F12", 0), ("F13", 0), ("F14", 0), ("F15", 0), ("F16", 0), ("F17", 0), ("Acf", 0), ("Tfp", 0)])
+CHER_TO_COUNTS = CHEB_TO_COUNTS.copy()
+MCP_TO_COUNTS = OrderedDict([("$total", 0), ("64H", 0), ("58H", 0), ("52H", 0), ("48H", 0), ("44H", 0), ("42H", 0), ("40H", 0), ("38H", 0), ("36H", 0), ("34H", 0), ("28H", 0), ("24H", 0)]) 
+
+CHEA_FILE_OUTPUT = "CheA_across_genomes.txt"
+CHEB_FILE_OUTPUT = "CheB_across_genomes.txt"
+CHER_FILE_OUTPUT = "CheR_across_genomes.txt"
+CHEZ_FILE_OUTPUT = "CheZ_across_genomes.txt"
+CHED_FILE_OUTPUT = "CheD_across_genomes.txt"
+CHEV_FILE_OUTPUT = "CheV_across_genomes.txt"
+MCP_FILE_OUTPUT = "MCP_across_genomes.txt"
+
+OUTPU_FILE_TO_DICT = OrderedDict([(CHEA_FILE_OUTPUT, CHEA_TO_COUNTS), (CHEB_FILE_OUTPUT, CHEB_TO_COUNTS), (CHER_FILE_OUTPUT, CHER_TO_COUNTS), (CHEZ_FILE_OUTPUT, CHEZ_TO_COUNTS), (CHED_FILE_OUTPUT, CHED_TO_COUNTS), (CHEV_FILE_OUTPUT, CHEV_TO_COUNTS), (MCP_FILE_OUTPUT, MCP_TO_COUNTS) ])
+COMPONENT_TO_DATASTRUCTURE = dict([("chea", CHEA_TO_COUNTS), ("chew", CHEA_TO_COUNTS), ("checx", CHEA_TO_COUNTS), ("other", CHEA_TO_COUNTS), ("chev", CHEV_TO_COUNTS), ("cheb", CHEB_TO_COUNTS), ("cher", CHER_TO_COUNTS), ("ched", CHED_TO_COUNTS), ("chez", CHEZ_TO_COUNTS), ("mcp", MCP_TO_COUNTS)]) 
+
+
+def collectSignalGenesCounts():
+	with open(OUTPUT_FILE, 'w') as output_file:
+		output_file.write( "\t".join('GenomeId, Total, OCP_total, TCP_total, TCP_HK, TCP_HHK, TCP_RR, TCP_HRR, TCP_Other, ChemSys, ECF, Other, \n'.split(",")) )
+	for outpufFile, data_structure in OUTPU_FILE_TO_DICT.items():
+		with open(outpufFile, 'w') as outpuf_file:
+			outpuf_file.write("GenomeId\t" + "\t".join(data_structure.keys()) + "\n")
+	
+	
+	recordCounter = 0
+	with open(INPUT_FILE) as inputfile:
+		for line in inputfile:
+			genomeId = line.strip()
+			recordCounter+=1
+			print((genomeId + "\t" + str(recordCounter)))		
+			for iteration in range(0, 10):
+				try:
+					conn = http.client.HTTPSConnection("api.mistdb.caltech.edu")
+					conn.request("GET", "/v1/genomes/" + genomeId + "/stp-matrix?per_page=1")
+					result = conn.getresponse()
+					data = json.loads(result.read().decode("utf-8"))
+					if len(data) == 0:   #No data anymore from this page on
+						break
+					if "name" in data:	#404 NotFoundError
+						break
+				except ValueError: #504 Gateway timeout
+					if iteration == 9:
+						with open (TIMEOUT_FILE, "a") as timeoutFile:
+							timeoutFile.write(genomeId + "\n")
+					continue
+					
+				total = OCP_total = TCP_total = TCP_HK = TCP_HHK = TCP_RR = TCP_HRR = TCP_Other = ECF = ChemSys = Other = "0"
+				if "counts" in data:
+					if "$total" in data["counts"]:
+						total = data["counts"]["$total"]
+					if "ocp" in data["counts"]:     
+						OCP_total = data["counts"]["ocp"]["$total"]
+					if "tcp" in data["counts"]: 
+						TCP_total = data["counts"]["tcp"]["$total"]
+						if "hk" in data["counts"]["tcp"]:
+							TCP_HK = data["counts"]["tcp"]["hk"]["$total"]
+						if "hhk" in data["counts"]["tcp"]:      
+							TCP_HHK = data["counts"]["tcp"]["hhk"]["$total"]
+						if "rr" in data["counts"]["tcp"]:       
+							TCP_RR = data["counts"]["tcp"]["rr"]["$total"]
+						if "hrr" in data["counts"]["tcp"]:      
+							TCP_HRR = data["counts"]["tcp"]["hrr"]["$total"]
+						if "other" in data["counts"]["tcp"]:            
+							TCP_Other = data["counts"]["tcp"]["other"]["$total"]
+					if "chemotaxis" in data["counts"]:						
+						if "chea" in data["counts"]["chemotaxis"]:
+							#This is for the one file output with all ST proteins and the number of chemotaxis systems
+							ChemSys = data["counts"]["chemotaxis"]["chea"]["$total"]						
+						for component, data_strcuture in COMPONENT_TO_DATASTRUCTURE.items():
+							processChemotaxisSystems(data, component, data_strcuture)
+
+					if "ecf" in data["counts"]:
+						ECF = data["counts"]["ecf"]["$total"]
+					if "other" in data["counts"]:
+						Other = data["counts"]["other"]["$total"]
+				
+				for outputFile, dataDict in OUTPU_FILE_TO_DICT.items():
+					saveToFile(dataDict, outputFile, genomeId)
+					resetDataStructure(dataDict)
+				with open(OUTPUT_FILE, 'a') as output_file:
+					output_file.write( "\t".join(map(str, ([genomeId, total, OCP_total, TCP_total, TCP_HK, TCP_HHK, TCP_RR, TCP_HRR, TCP_Other, ChemSys, ECF, Other]))) + "\n") 
+				 
+						
+				break
+
+def processChemotaxisSystems(data, entity, component_to_counts = False):
+	#entity can be chea, mcp, cheb, cher, and so on
+	additional = set(["checx", "chew", "other"])
+	if entity in data["counts"]["chemotaxis"]:
+		for system, subdata in data["counts"]["chemotaxis"][entity].items():
+			if system == "$total":
+				if entity not in additional:
+					component_to_counts[system] = subdata
+				else:
+					component_to_counts[entity] = subdata
+			elif system in component_to_counts:
+				component_to_counts[system] = subdata["$total"]	
+
+def  saveToFile(dataDict, outputFile, genomeId):
+	with open(outputFile, 'a') as output_file:
+		output_file.write(genomeId + "\t" + "\t".join(map(str, (dataDict.values()))) + "\n")
+
+def resetDataStructure(dataStrcture):
+	for key in dataStrcture:
+		dataStrcture[key] = 0
+
+def main(argv):
+	initialize(argv)
+	collectSignalGenesCounts()
+
+	
+main(sys.argv)
+	
+
+
+	
+
+
+
+
+
+
+
+
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
